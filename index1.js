069b911b4e46eb08af9923dbab2c3b05fc906c1c
unknown <xi-qian.fan@goldpac.com>
2018-12-06 13:54:31 +0800

    调整代码

--- index1.js
+++ index1.js
@@ -1,87 +1,98 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const path = require('path');
-const builder = require('xmlbuilder');
-const progressBar = require('progress');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const builder = require("xmlbuilder");
+const progressBar = require("progress");
 let bar;
 let index = 0;
 let questions = [{
-        type: 'input',
-        name: 'snippet_file',
-        message: 'path to the snippet file of vscode extensions',
+        type: "input",
+        name: "snippet_file",
+        message: "path to the snippet file of vscode extensions",
         validate: function (val) {
             if (!fs.existsSync(val)) {
-                return 'Please enter a valid path';
+                return "Please enter a valid path";
             }
-            if (!fs.statSync(val).isFile() || path.extname(val).toLowerCase() !== '.json') {
-                return 'Please enter a valid snippet file path(warning:snippet file must be end with \'.json\')';
+            if (
+                !fs.statSync(val).isFile() ||
+                path.extname(val).toLowerCase() !== ".json"
+            ) {
+                return "Please enter a valid snippet file path(warning:snippet file must be end with '.json')";
             }
             return true;
         }
     },
     {
-        type: 'input',
-        name: 'des_folder',
-        message: 'folder to store the converted snippet files(for vs)',
-        default: './',
+        type: "input",
+        name: "des_folder",
+        message: "folder to store the converted snippet files(for vs)",
+        default: "./",
         validate: function (val) {
             if (!fs.existsSync(val)) {
-                return 'Please enter a valid path'
+                return "Please enter a valid path";
             }
             return true;
         }
     },
     {
-        type: 'list',
-        name: 'lang',
-        message: 'Which language is your snippet?',
-        choices: [
-            'html',
-            'JavaScript'
-        ]
+        type: "list",
+        name: "lang",
+        message: "Which language is your snippet?",
+        choices: ["html", "JavaScript"]
     }
 ];
 
 function creactSnippetFile(snippetItem, desPath, lang) {
-    var root = builder.create('CodeSnippet');
-    root.att('Format', '1.1.0');
-    root.att('xmlns', 'http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet');
-    var header = root.ele('Header');
-    var title = header.ele('Title');
+    var root = builder.create("CodeSnippet");
+    root.att("Format", "1.1.0");
+    root.att(
+        "xmlns",
+        "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet"
+    );
+    var header = root.ele("Header");
+    var title = header.ele("Title");
     title.text(snippetItem.prefix);
-    var author = header.ele('Author');
-    author.text('fanxq');
-    var shortcut = header.ele('Shortcut');
+    var author = header.ele("Author");
+    author.text("fanxq");
+    var shortcut = header.ele("Shortcut");
     shortcut.text(snippetItem.prefix);
-    var description = header.ele('Description');
+    var description = header.ele("Description");
     description.text(snippetItem.description);
-    var snippetTypes = header.ele('SnippetTypes');
-    snippetTypes.ele('SnippetType', null, 'Expansion');
-    snippetTypes.ele('SnippetType', null, 'SurroundsWith');
-    var snippet = root.ele('Snippet');
-    var declarations = snippet.ele('Declarations');
-    var reg = /(\$\d+)|(\$\{[\d\w:]{1,}\})/g;
-    var strcode = snippetItem.body.toString();
-    var matchs = strcode.match(reg);
-    var baseId = 'content';
+    var snippetTypes = header.ele("SnippetTypes");
+    snippetTypes.ele("SnippetType", null, "Expansion");
+    snippetTypes.ele("SnippetType", null, "SurroundsWith");
+    var snippet = root.ele("Snippet");
+    var declarations = snippet.ele("Declarations");
+    var snippetBody = snippetItem.body.toString();
+    var matchs = snippetBody.match(/(\$\d+)|(\$\{[^\{\}]*\})/gim);
     if (matchs) {
         for (let i = 0; i < matchs.length; i++) {
-            var lId = baseId + i;
-            var literal = declarations.ele('Literal');
-            literal.ele('ID').text(lId);
-            var tempstr = matchs[i];
-            var literalArray = /\$\{([\d\w:]{1,})\}/g.exec(tempstr);
-            if (literalArray && literalArray.length > 1) {
-                literal.ele('ToolTip').text(literalArray[1]);
-                literal.ele('Default').text(literalArray[1]);
+            var literalId = "Literal" + i;
+            var literal = declarations.ele("Literal");
+            var literalTooltip = 'none';
+            var literalDefault = 'none';
+            var result = /\$((\d+)|\{(.*)\})/gi.exec(matchs[i]);
+            if (result && result.length > 0) {
+                var text = result[2] || result[3];
+                if (text) {
+                    literalTooltip = text;
+                    literalDefault = text;
+                    if (~text.indexOf(":")) {
+                        var texts = text.split(":");
+                        literalId = texts[0];
+                        literalTooltip = literalDefault = texts[1];
+                    }
+                }
             }
-            var rStr = '$' + lId + '$';
-            strcode = strcode.replace(matchs[i], rStr);
+            literal.ele("ID").text(literalId);
+            literal.ele("ToolTip").text(literalTooltip);
+            literal.ele("Default").text(literalDefault);
+            snippetBody = snippetBody.replace(matchs[i], "$" + literalId + "$");
         }
     }
 
-    var code = snippet.ele('Code').att('Language', lang); 
-    code.cdata(strcode + '$end$');
+    var code = snippet.ele("Code").att("Language", lang);
+    code.cdata(snippetBody + "$end$");
     root.end({
         pretty: true
     });
@@ -89,19 +100,18 @@ function creactSnippetFile(snippetItem, desPath, lang) {
     if (!fs.existsSync(desPath)) {
         fs.mkdirSync(desPath);
     }
-    // fs.writeFile(path.join(desPath, tFileName), root, function (err) {
-    //     if (err) {
-    //         console.log(err.message);
-    //     }
-    // });
-    fs.writeFileSync(path.join(desPath, tFileName),root,{encoding:'utf8'});
+    fs.writeFileSync(path.join(desPath, tFileName), root, {
+        encoding: "utf8"
+    });
     bar.tick(index);
     index++;
 }
 
 inquirer.prompt(questions).then(function (answers) {
-    //console.log(answers);
-    var outputPath = path.join(answers.des_folder, answers.lang === 'html' ? 'html.snippets' : 'javascript.snippets');
+    var outputPath = path.join(
+        answers.des_folder,
+        answers.lang === "html" ? "html.snippets" : "javascript.snippets"
+    );
     fs.mkdirSync(outputPath);
     fs.readFile(answers.snippet_file, function (err, data) {
         if (err) {
@@ -110,17 +120,16 @@ inquirer.prompt(questions).then(function (answers) {
         }
 
         var snippetItems = JSON.parse(data.toString());
-        bar = new progressBar('  downloading [:bar] :percent ', {
-            complete: '=',
-            incomplete: ' ',
-            width: 20,
+        bar = new progressBar("  converting [:bar] :percent ", {
+            complete: "=",
+            incomplete: " ",
+            width: 50,
             total: Object.keys(snippetItems).length
-          });
+        });
 
         for (var i in snippetItems) {
             //console.log(i);
             creactSnippetFile(snippetItems[i], outputPath, answers.lang);
         }
     });
-
-})
+});
