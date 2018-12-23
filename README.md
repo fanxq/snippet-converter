aa96bb38c150e1c862ace07e2dcfcfbe05e0644b
fan <1508422424@qq.com>
2018-12-23 22:50:44 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## 为什么写这个工具？
-因为平时工作时使用的开发工具为vscode和vs2015，在使用vscode时，装了一些代码片段插件，在敲代码时只需敲代码片段的前缀（prefix）就会自动带出整个代码片段，这样也就无须记忆太多语法或单词，提高开发效率。于是在使用vs2015时，我就想着是否能够把vscode的代码片段带到vs2015中使用。在网上搜索了一番，发现vs2015有自定义代码片段的功能，所以我只要把vscode的代码片段转换为vs2015可以使用的代码片段即可。
+因为平时工作时使用的开发工具为vscode和vs2015，在使用vscode时，装了一些代码片段插件，在敲代码时只需敲代码片段的前缀（prefix）就会自动带出整个代码片段，这样也就无须记忆太多语法或单词，提高开发效率。于是在使用vs2015时，我就想着是否能够把vscode的代码片段带到vs2015中使用。因为vs2015有自定义代码片段的功能，所以我只要把vscode的代码片段转换为vs2015可以使用的代码片段即可。
 <br/>
 vscode插件中的代码片段为json格式，至于如何定义vscode的代码片段，请查看vscode的文档：[Creating your own snippets](https://code.visualstudio.com/docs/editor/userdefinedsnippets)，文中介绍到snippet的语法基本遵循[TextMate snippet](https://macromates.com/manual/en/snippets)的语法。
 如下所示：
@@ -42,6 +42,7 @@ vs2015的代码片段则为xml格式，扩展名为.snippet的文件，如下所
   </Snippet>
 </CodeSnippet>
 ```
+
 该项目使用node.js编写，依赖的库有
 <br/>
 - xmlbuilder
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
@@ -6,133 +7,135 @@ const builder = require("xmlbuilder");
 const progressBar = require("progress");
 let bar;
 let index = 0;
-let questions = [
-  {
-    type: "input",
-    name: "snippet_file",
-    message: "path to the snippet file of vscode extensions",
-    validate: function(val) {
-      if (!fs.existsSync(val)) {
-        return "Please enter a valid path";
-      }
-      if (
-        !fs.statSync(val).isFile() ||
-        path.extname(val).toLowerCase() !== ".json"
-      ) {
-        return "Please enter a valid snippet file path(warning:snippet file must be end with '.json')";
-      }
-      return true;
+let questions = [{
+        type: "input",
+        name: "snippet_file",
+        message: "path to the snippet file of vscode extensions",
+        validate: function (val) {
+            if (!fs.existsSync(val)) {
+                return "Please enter a valid path";
+            }
+            if (
+                !fs.statSync(val).isFile() ||
+                path.extname(val).toLowerCase() !== ".json"
+            ) {
+                return "Please enter a valid snippet file path(warning:snippet file must be end with '.json')";
+            }
+            return true;
+        }
+    },
+    {
+        type: "input",
+        name: "des_folder",
+        message: "folder to store the converted snippet files(for vs)",
+        default: "./snippets",
+        validate: function (val) {
+            if (val === "./snippets") {
+                return true;
+            }
+            if (!fs.existsSync(val)) {
+                return "Please enter a valid path";
+            }
+            return true;
+        }
+    },
+    {
+        type: "list",
+        name: "lang",
+        message: "language of your snippet?",
+        choices: ["html", "JavaScript"]
     }
-  },
-  {
-    type: "input",
-    name: "des_folder",
-    message: "folder to store the converted snippet files(for vs)",
-    default: "./snippets",
-    validate: function(val) {
-      if (val === "./snippets") {
-        return true;
-      }
-      if (!fs.existsSync(val)) {
-        return "Please enter a valid path";
-      }
-      return true;
-    }
-  },
-  {
-    type: "list",
-    name: "lang",
-    message: "language of your snippet?",
-    choices: ["html", "JavaScript"]
-  }
 ];
 
 function creactSnippetFile(snippetItem, desPath, lang) {
-  var root = builder.create("CodeSnippet");
-  root.att("Format", "1.1.0");
-  root.att(
-    "xmlns",
-    "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet"
-  );
-  var header = root.ele("Header");
-  var title = header.ele("Title");
-  title.text(snippetItem.prefix);
-  var author = header.ele("Author");
-  author.text("fanxq");
-  var shortcut = header.ele("Shortcut");
-  shortcut.text(snippetItem.prefix);
-  var description = header.ele("Description");
-  description.text(snippetItem.description);
-  var snippetTypes = header.ele("SnippetTypes");
-  snippetTypes.ele("SnippetType", null, "Expansion");
-  snippetTypes.ele("SnippetType", null, "SurroundsWith");
-  var snippet = root.ele("Snippet");
-  var declarations = snippet.ele("Declarations");
-  var snippetBody = snippetItem.body.toString();
-  var matchs = snippetBody.match(/(\$\d+)|(\$\{[^\{\}]*\})/gim);
-  if (matchs) {
-    for (let i = 0; i < matchs.length; i++) {
-      var literalId = "Literal" + i;
-      var literal = declarations.ele("Literal");
-      var literalTooltip = "none";
-      var literalDefault = "-";
-      var result = /\$((\d+)|\{(.*)\})/gi.exec(matchs[i]);
-      if (result && result.length > 0) {
-        var text = result[2] || result[3];
-        if (text) {
-          if (~text.indexOf(":")) {
-            var texts = text.split(":");
-            literalId = texts[0];
-            literalTooltip = literalDefault = texts[1];
-          } else if (!Number.isNaN(Number(text))) {
-            literalId = text;
-          } else {
-            literalTooltip = literalDefault = text;
-          }
+    var root = builder.create("CodeSnippet");
+    root.att("Format", "1.1.0");
+    root.att(
+        "xmlns",
+        "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet"
+    );
+    var header = root.ele("Header");
+    var title = header.ele("Title");
+    title.text(snippetItem.prefix);
+    var author = header.ele("Author");
+    author.text("fanxq");
+    var shortcut = header.ele("Shortcut");
+    shortcut.text(snippetItem.prefix);
+    var description = header.ele("Description");
+    description.text(snippetItem.description);
+    var snippetTypes = header.ele("SnippetTypes");
+    snippetTypes.ele("SnippetType", null, "Expansion");
+    snippetTypes.ele("SnippetType", null, "SurroundsWith");
+    var snippet = root.ele("Snippet");
+    var declarations = snippet.ele("Declarations");
+    var snippetBody = Object.prototype.toString.call(snippetItem.body) === "[object Array]" ? snippetItem.body.join('\n') : snippetItem.body.toString();
+    var matchs = snippetBody.match(/(\$\d+)|(\$\{[^\{\}]*\})/gim);
+    if (matchs) {
+        for (let i = 0; i < matchs.length; i++) {
+            var literalId = "Literal" + i;
+            var literal = declarations.ele("Literal");
+            var literalTooltip = "none";
+            var literalDefault = "-";
+            var result = /\$((\d+)|\{(.*)\})/gi.exec(matchs[i]);
+            if (result && result.length > 0) {
+                var text = result[2] || result[3];
+                if (text) {
+                    if (~text.indexOf(":")) {
+                        var texts = text.split(":");
+                        literalId = texts[0];
+                        literalTooltip = literalDefault = texts[1];
+                    } else if (!Number.isNaN(Number(text))) {
+                        literalId = text;
+                    } else {
+                        literalTooltip = literalDefault = text;
+                    }
+                }
+            }
+            literal.ele("ID").text(literalId);
+            literal.ele("ToolTip").text(literalTooltip);
+            literal.ele("Default").text(literalDefault);
+            snippetBody = snippetBody.replace(matchs[i], "$" + literalId + "$");
         }
-      }
-      literal.ele("ID").text(literalId);
-      literal.ele("ToolTip").text(literalTooltip);
-      literal.ele("Default").text(literalDefault);
-      snippetBody = snippetBody.replace(matchs[i], "$" + literalId + "$");
     }
-  }
 
-  var code = snippet.ele("Code").att("Language", lang);
-  code.cdata(snippetBody + "$end$");
-  root.end({
-    pretty: true
-  });
-  var tFileName = snippetItem.prefix.toString() + ".snippet";
-  if (!fs.existsSync(desPath)) {
-    fs.mkdirSync(desPath);
-  }
-  fs.writeFileSync(path.join(desPath, tFileName), root, {
-    encoding: "utf8"
-  });
-  bar.tick(index);
-  index++;
+    var code = snippet.ele("Code").att("Language", lang);
+    code.cdata(snippetBody + "$end$");
+    root.end({
+        pretty: true
+    });
+    var tFileName = snippetItem.prefix.toString() + ".snippet";
+    if (!fs.existsSync(desPath)) {
+        fs.mkdirSync(desPath);
+    }
+    fs.writeFileSync(path.join(desPath, tFileName), root, {
+        encoding: "utf8"
+    });
+    bar.tick(index);
+    if (index === bar.total) {
+        bar.interrupt("Done");
+    }
+    index++;
 }
 
-inquirer.prompt(questions).then(function(answers) {
-  answers.des_folder === "./snippets" && fs.mkdirSync(answers.des_folder);
-  fs.readFile(answers.snippet_file, function(err, data) {
-    if (err) {
-      console.log(err.message);
-      return;
-    }
+inquirer.prompt(questions).then(function (answers) {
+    answers.des_folder === "./snippets" && fs.mkdirSync(answers.des_folder);
+    fs.readFile(answers.snippet_file, function (err, data) {
+        if (err) {
+            console.log(err.message);
+            return;
+        }
 
-    var snippetItems = JSON.parse(data.toString());
-    bar = new progressBar("  converting [:bar] :percent ", {
-      complete: "=",
-      incomplete: " ",
-      width: 50,
-      total: Object.keys(snippetItems).length
+        var snippetItems = JSON.parse(data.toString());
+        bar = new progressBar("Converting [:bar] :percent ", {
+            complete: "=",
+            incomplete: " ",
+            width: 50,
+            total: Object.keys(snippetItems).length
+        });
+
+        for (var i in snippetItems) {
+            //console.log(i);
+            creactSnippetFile(snippetItems[i], answers.des_folder, answers.lang);
+        }
     });
-
-    for (var i in snippetItems) {
-      //console.log(i);
-      creactSnippetFile(snippetItems[i], answers.des_folder, answers.lang);
-    }
-  });
 });
